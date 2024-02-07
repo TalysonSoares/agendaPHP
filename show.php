f54def2cd8345c2dae45080a671e97320c91898d
TalysonSoares <talysonsoares40@gmail.com>
2024-02-07 12:32:05 -0300

    CSS da pagina de detalhes

--- show.php
+++ show.php
@@ -2,6 +2,7 @@
     include_once("templates/header.php");
 ?>
     <div class="container" id="viewContactContainer">
+        <?php include_once("templates/backBtn.html") ?>
         <h1 id="mainTitle"><?= $contact["name"] ?></h1>
         <p class="bold">Telefone:</p>
         <p><?= $contact["phone"] ?></p>
