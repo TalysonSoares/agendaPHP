2c15fe64f55cadd6e8b37076c1e2dbcef0e61dba
TalysonSoares <talysonsoares40@gmail.com>
2024-02-07 12:03:15 -0300

    Criação da tela de detalhes do contato

---
+++ show.php
@@ -0,0 +1,13 @@
+<?php
+    include_once("templates/header.php");
+?>
+    <div class="container" id="viewContactContainer">
+        <h1 id="mainTitle"><?= $contact["name"] ?></h1>
+        <p class="bold">Telefone:</p>
+        <p><?= $contact["phone"] ?></p>
+        <p class="bold">Observações:</p>
+        <p><?= $contact["observations"] ?></p>
+    </div>
+<?php
+    include_once("templates/footer.php");
+?>
