5c99c61fa2e235db65b9c72600201944e2849ec2
TalysonSoares <talysonsoares40@gmail.com>
2024-02-09 12:29:31 -0300

    Criando tela de cadastro de contato

--- create.php
+++ create.php
@@ -1,8 +1,22 @@
 <?php
     include_once("templates/header.php");
 ?>
-  <h1>Adicionando contato</h1>
-  <i class="fas fa-eye"></i>
+  <div class="container">
+    <?php include_once("templates/backBtn.html") ?>
+    <h1 id="mainTitle">Criar Contato</h1>
+    <form action="<?= $BASE_URL ?>config\process.php" method="POST">
+      <input type="hidden" name="type" value="create">
+      <div class="mb-3">
+        <label for="name" class="form-label">Nome do contato:</label>
+        <input type="text" class="form-control" name="name" id="name" placeholder="Digite o nome" required>
+      </div>
+      <div class="mb-3">
+        <label for="phone" class="form-label">Telefone do contato:</label>
+        <input type="text" class="form-control" name="phone" id="phone" placeholder="Digite o telefone" required>
+      </div>
+      <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </form>
+  </div>
 <?php
     include_once("templates/footer.php");
 ?> 
